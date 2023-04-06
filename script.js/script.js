0528b2b14c283de42fc9f979333e8cf092d42d2a
Your Name <you@example.com>
2023-04-05 17:07:02 -0700

    change background color when clicked

---
+++ script.js/script.js
@@ -0,0 +1,5 @@
+let buttonRef = document.querySelector(".DarkTheme");
+function backgroundChange(){
+    document.body.classList.add("changetodark")
+}
+buttonRef.addEventListener('click',backgroundChange)
