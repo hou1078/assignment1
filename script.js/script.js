6837d5cfa7e23147b815fa5d6e9e28fb526d4f37
Your Name <you@example.com>
2023-04-05 18:35:38 -0700

    finish save button and check the notes contents

--- script.js/script.js
+++ script.js/script.js
@@ -41,18 +41,31 @@ function Visiblity() {
 newBtn.addEventListener("click",Visiblity)
 
 const notesArray = [
-    {title: "note one", body: "this is my first note"}
+    {title: "note one", body: "this is my first note"},
+    {title: "note two", body: "this is my second note"}
   ];
-notes=[]
-
-function AddNotes(){
+  
+  const sidebar = document.querySelector(".AddList");
+  
+  function addNote() {
     const title = prompt("Please enter the title of your note:");
-    const body= document.querySelector("#textarea").value;
-    note={title:title, body:body}
-    notes.push(note);
-    const sidebar = document.querySelector(".AddList");
+    const body = textarea.value;
+    const note = {title: title, body: body};
+    notesArray.push(note);
     const noteList = document.createElement("li");
     noteList.textContent = title;
     sidebar.appendChild(noteList);
-}
-saveBtn.addEventListener("click", AddNotes)
+  }
+  
+  document.querySelector(".Save").addEventListener("click", addNote);
+  
+  function displayNoteBody(event) {
+    if (event.target.tagName === "LI") {
+      const clickedTitle = event.target.textContent;
+      const clickedNote = notesArray.find(note => note.title === clickedTitle);
+      textarea.value = clickedNote.body;
+    }
+  }
+  
+  sidebar.addEventListener("click", displayNoteBody);
+  
